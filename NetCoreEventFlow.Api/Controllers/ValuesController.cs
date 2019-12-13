3a3fea28133d0193f32c65018357f3c526c952c0
Paweł Wichary <wicharypawel@gmail.com>
2019-12-13 13:06:32 +0100

    Scaffold solution & cleanup

---
+++ NetCoreEventFlow.Api/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NetCoreEventFlow.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello world");
+        }
+    }
+}
